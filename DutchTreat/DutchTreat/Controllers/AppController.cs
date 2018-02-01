ac8e1d8b7828d34c3af9fb23cabd60092416dffb
vuta1927 <35493980+vuta1927@users.noreply.github.com>
2018-02-02 00:58:11 +0700

    Dutch Treat

---
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DutchTreat.Data;
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly DutchContext _context;
+        public AppController(DutchContext context){
+            _context = context;
+        }
+        public IActionResult Index(){
+            return View();
+        }
+
+        public IActionResult Shop(){
+            // var result = _context.Products.OrderBy(p=>p.Category).ToList();
+            var result = from p in _context.Products
+                            orderby p.Category select p;
+            return View(result.ToList());
+        }
+    }
+}
---
+++ DutchTreat/DutchTreat/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using DutchTreat.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace DutchTreat
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config){
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DutchContext>(cfg =>{
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+            });
+            services.AddTransient<DutchSeeder>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if(env.IsDevelopment()){
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseStaticFiles();
+
+            app.UseMvc(cfg =>{
+                cfg.MapRoute("Default","{controller}/{action}/{id?}", new { 
+                    controller = "App", Action="Index"});
+                cfg.MapRoute("Shop","{controller}/{action}/{id?}", new { 
+                    controller = "App", Action="Shop"});
+            });
+
+            if(env.IsDevelopment()){
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+                    seeder.Seed();
+                }
+            }
+        }
+    }
+}
